6f440bb039fce5fcb6f3695838c412fd9f780bd2
proggeler <bas@dmt-software.nl>
2024-11-13 16:02:55 +0100

    #7: fix deserialize with empty DateTime values in generated CustomFields entity

--- tests/Entity/SubscriberTest.php
+++ tests/Entity/SubscriberTest.php
@@ -3,6 +3,7 @@
 namespace DMT\Test\Laposta\Api\Entity;
 
 use DMT\Laposta\Api\Config;
+use DMT\Laposta\Api\Entity\CustomField;
 use DMT\Laposta\Api\Entity\Subscriber;
 use DMT\Laposta\Api\Factories\SerializerFactory;
 use DMT\Test\Laposta\Api\Fixtures\CustomFields;
@@ -30,5 +31,76 @@ class SubscriberTest extends TestCase
         $this->assertStringContainsString('email=test%40example.org', $payload);
         $this->assertStringContainsString('ip=127.0.0.1', $payload);
         $this->assertStringContainsString('custom_fields%5Bname%5D=Jane+Do', $payload);
+    }
+
+    /** @dataProvider provideSubscriberJson */
+    public function testDeserializeSubscriberWithCustomFieldsEntity(
+        string $subscriberJson,
+        string $emptyField,
+        $expectedEmptyValue
+    ) {
+        $config = Config::fromArray([
+            'customFieldsClasses' => [
+                'BaImMu3JZA' => CustomFields::class,
+            ],
+        ]);
+
+        $subscriber = SerializerFactory::create($config)->deserialize(
+            $subscriberJson,
+            Subscriber::class,
+            'json'
+        );
+
+        $this->assertInstanceOf(Subscriber::class, $subscriber);
+        $this->assertInstanceOf(CustomFields::class, $subscriber->customFields);
+        $this->assertSame($expectedEmptyValue, $subscriber->customFields->$emptyField);
+    }
+
+    /** @dataProvider provideSubscriberJson */
+    public function testDeserializeSubscriberWithoutCustomFieldsEntity(string $subscriberJson, string $emptyField)
+    {
+        $subscriber = SerializerFactory::create(new Config())->deserialize(
+            $subscriberJson,
+            Subscriber::class,
+            'json'
+        );
+
+        $this->assertInstanceOf(Subscriber::class, $subscriber);
+        $this->assertContainsOnlyInstancesOf(CustomField::class, $subscriber->customFields->field);
+
+        $fields = [];
+        array_walk(
+            $subscriber->customFields->field,
+            function(CustomField $field) use (&$fields) {
+                $fields[$field->name] = $field;
+            }
+        );
+
+        $this->assertEmpty($fields[$emptyField]->value);
+    }
+
+    public function provideSubscriberJson(): iterable
+    {
+        $json = file_get_contents(__DIR__ . '/../Fixtures/subscriber.json');
+
+        $subscriber = json_decode($json, true, 512, JSON_OBJECT_AS_ARRAY);
+        $subscriber['member']['custom_fields']['name'] = "";
+
+        yield 'json with empty string' => [json_encode($subscriber), 'name', ""];
+
+        $subscriber = json_decode($json, true, 512, JSON_OBJECT_AS_ARRAY);
+        $subscriber['member']['custom_fields']['dateofbirth'] = "";
+
+        yield 'json with empty datetime' => [json_encode($subscriber), "dateofbirth", null];
+
+        $subscriber = json_decode($json, true, 512, JSON_OBJECT_AS_ARRAY);
+        $subscriber['member']['custom_fields']['children'] = "";
+
+        yield 'json with empty integer' => [json_encode($subscriber), "children", 0];
+
+        $subscriber = json_decode($json, true, 512, JSON_OBJECT_AS_ARRAY);
+        $subscriber['member']['custom_fields']['prefs'] = [];
+
+        yield 'json with empty array' => [json_encode($subscriber), 'prefs', []];
     }
 }
